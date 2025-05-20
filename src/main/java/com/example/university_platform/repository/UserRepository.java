950027fcf07bb41621e6064b568c309ae34f0bb9
Arystuna <aristanaslanov@mail.ru>
2025-05-20 16:59:07 +0500

    добавление аннотации в UserRepository

--- src/main/java/com/example/university_platform/repository/UserRepository.java
+++ src/main/java/com/example/university_platform/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.example.university_platform.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 }
