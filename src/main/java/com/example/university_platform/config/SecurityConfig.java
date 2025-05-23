1f1676e779c02f00757d57c9cab6fd12a60477bd
Arystuna <aristanaslanov@mail.ru>
2025-05-23 19:58:11 +0500

    для новой части кода

--- src/main/java/com/example/university_platform/config/SecurityConfig.java
+++ src/main/java/com/example/university_platform/config/SecurityConfig.java
@@ -4,12 +4,16 @@ package com.example.university_platform.config;
 import com.example.university_platform.service.CustomUserDetailsService; // Импортируем наш сервис
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
